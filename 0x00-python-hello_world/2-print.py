318eb58bf92d69ca773b5d3661c0e62d2a692f11
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-10-31 09:17:58 +0300

    done number 2

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-# Use the print function to display the message
-print("Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")

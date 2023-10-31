3ada9574c25ff224ea5098405589f59de9a7613f
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 01:27:08 +0300

    made an array and checked inside it

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for d in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
-        if (d == c):
+    d = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if c in d:
             return True
     return False

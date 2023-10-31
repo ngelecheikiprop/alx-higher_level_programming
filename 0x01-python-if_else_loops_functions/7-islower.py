6d7cbb359e9e435e563d32c1811974f130c4743b
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 01:16:20 +0300

    completed task 7 on making a function to check if letter is uppercase or lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    for d in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
+        if (d == c):
+            return True
+    return False

81478c762c641956343a1edb6553552c38f04080
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 10:30:25 +0300

    made the last one not have a comma

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 j = 1
-for i in range(0,100):
-	if (i < 99):
-		if ((i % 10) == 0):
-			i = i + j
-			j+=1
-		print("{:02d}".format(i), end=', ')
-	else:
-		print("{:02d}".format(i), end='\n')
+i = 0
+while (i <= 100):
+    if (i < 99):
+        if ((i % 10) == 0):
+            i = i + j
+            j += 1
+            if (i >= 100):
+                continue
+            if (i == 89):
+                print("{:02d}".format(i), end='\n')
+                break
+        print("{:02d}".format(i), end=', ')
+    i += 1

19e269f53228aa2b47debf3df2fea52bf9cf7673
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 14:42:00 +0300

    made last digiit negative for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 numcpy = number
-if (numcpy < 0):
-    numcpy = numcpy * -1
-lst_dgt = numcpy % 10
+lst_dgt = abs(number) % 10
+if number < 0:
+    lst_dgt = lst_dgt * -1
 if (lst_dgt > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, lst_dgt), end="\n")
 elif (lst_dgt == 0):

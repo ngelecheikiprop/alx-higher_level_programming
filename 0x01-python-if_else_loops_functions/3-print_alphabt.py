0421cdd11cba80a06d94bbd5fef4bfe25f587281
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 15:01:41 +0300

    debugged to not print e and d

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if (c == 'e') or (c == 'q'):
+    if (c == ord('e')) or (c == ord('q')):
         continue
     print("{}".format(chr(c)),  end='')

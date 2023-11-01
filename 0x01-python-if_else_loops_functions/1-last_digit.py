1543895d5e0b1bf89270ffed775d70f837a791c3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 14:52:40 +0300

    removd the errors on coding style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,10 @@ lst_dgt = abs(number) % 10
 if number < 0:
     lst_dgt = lst_dgt * -1
 if (lst_dgt > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, lst_dgt), end="\n")
+    print("Last digit of {} is {} and is greater than 5".format(
+        number, lst_dgt), end="\n")
 elif (lst_dgt == 0):
     print("Last digit of {} is {} and is 0".format(number, lst_dgt), end="\n")
 elif ((lst_dgt < 6) and (lst_dgt != 0)):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lst_dgt), end="\n")
+    print("Last digit of {} is {} and is less than 6 and not 0".format(
+        number, lst_dgt), end="\n")

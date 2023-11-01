f95ca73cdd1b07c19ef803f959521603e48bc75d
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 14:56:19 +0300

    removed coding style error on task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
-	if (c == 'e') or (c == 'q'):
-		continue
-	print("{}".format(c),  end='')
+for c in range(ord('a'), ord('z') + 1):
+    if (c == 'e') or (c == 'q'):
+        continue
+    print("{}".format(chr(c)),  end='')

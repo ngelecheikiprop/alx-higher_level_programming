bdfc7f65cae7d12f90a27a0fa1bc814c8ee4b736
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 10:37:39 +0300

    I changed the working of checking

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    d = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    if c in d:
-            return True
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False

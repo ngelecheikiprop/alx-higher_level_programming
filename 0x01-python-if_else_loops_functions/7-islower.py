ed12e464bf170a83090e8a8a1a75bb74b4997560
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 10:33:47 +0300

    I changed not to return false

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,4 +3,3 @@ def islower(c):
     d = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     if c in d:
             return True
-    return False

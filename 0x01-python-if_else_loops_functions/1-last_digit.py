faed9b10961c1a26bb138a3918c8c75a30b383c3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 08:53:42 +0300

    changed negative number to positive before finding last digit for negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lst_dgt = number % 10
+numcpy = number
+if (numcpy < 0):
+    numcpy = numcpy * -1
+lst_dgt = numcpy % 10
 if (lst_dgt > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, lst_dgt), end="\n")
 elif (lst_dgt == 0):

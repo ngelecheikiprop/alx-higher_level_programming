290f3ce0d74bc08418edecc77b4df969297a0e8b
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-01 06:33:10 +0300

    inserted spaces before and after variables

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ number = random.randint(-10000, 10000)
 lst_dgt = number % 10
 #check if lst_dgt > 5
 if (lst_dgt > 5):
-	print("Last digit of{}is{}and is greater than 5\n".format(number,lst_dgt))
+	print("Last digit of {} is {} and is greater than 5\n".format(number,lst_dgt))
 #check if == 0
 elif (lst_dgt == 0):
-	print("Last digit of{}is{}and is 0\n".format(number,lst_dgt))
+	print("Last digit of {} is {} and is 0\n".format(number,lst_dgt))
 #check if less than 6 and not 0
 elif ((lst_dgt < 6) and (lst_dgt != 0)):
-	print("Last digit of{}is{}and is less than 6 and not 0\n".format(number,lst_dgt))
+	print("Last digit of {} is {} and is less than 6 and not 0\n".format(number,lst_dgt))

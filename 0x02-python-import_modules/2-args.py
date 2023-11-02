64670dcab27decee73df4e27d1a1f345047a3bfa
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-03 01:36:28 +0300

    corrected printing errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     i = 0
     if leng == 1:
         print("{} arguments.".format(leng - 1), end="\n")
-    elif leng == 0:
-        print("{} argument".format(leng - 1), end="\n")
+    elif leng == 2:
+        print("{} argument:".format(leng - 1), end="\n")
     else:
-        print("{} argument".format(leng - 1), end="\n")
+        print("{} arguments:".format(leng - 1), end="\n")
     i = 1
     while (i < leng):
         print("{}: {}".format(i, arg[i]), end="\n")

70473781fd03ee7bdc6b6e6349c9b571910e69d9
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-03 20:15:34 +0300

    completed task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return my_list
+    if (idx > len(my_list) - 1):
+        return my_list
+    i = 0
+    while (i < len(my_list)):
+        if (i == idx):
+            my_list[i] = element
+            return my_list
+        i += 1

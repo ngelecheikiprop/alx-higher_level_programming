47a6bbe40a81114e10d6c11cee2f46f144f3c764
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-03 20:14:24 +0300

    completed task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+def new_in_list(my_list, idx, element):
+    my_copy = []
+    if (idx < 0):
+        return my_list
+    if (idx > len(my_list) - 1):
+        return my_list
+    i = 0
+    while (i < len(my_list)):
+        if (idx == i):
+            my_copy.append(element)
+            i += 1
+            continue
+        my_copy.append(my_list[i])
+        i += 1
+    return my_copy

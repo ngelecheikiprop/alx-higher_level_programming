f8a023ff695ba6fe93091ee81885995da9cb177a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-03 20:14:37 +0300

    completed task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    i = 0
+    j = -1
+    while (i < len(my_list)):
+        print("{}".format(my_list[j]), end="\n")
+        j -= 1
+        i += 1

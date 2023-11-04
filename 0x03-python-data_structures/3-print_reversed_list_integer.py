0e52d3bc8926c909f5f12873e6c0a1433e10bd03
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-04 17:40:06 +0300

    added :d to fomrat in task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     i = 0
     j = -1
     while (i < len(my_list)):
-        print("{}".format(my_list[j]), end="\n")
+        print("{:d}".format(my_list[j]), end="\n")
         j -= 1
         i += 1

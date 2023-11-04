8a41a1e80d8086b691d2452dd7b57feddf20e08b
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-05 01:40:09 +0300

    redone task 5
    this time I used looping while making
    a copy in task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.replace("c", "")
-    my_string = my_string.replace("C", "")
-    return my_string
+    i = 0
+    new_string = ""
+    while (i < len(my_string)):
+        if (my_string[i] == 'c'):
+            i += 1
+            continue
+        elif (my_string[i] == 'C'):
+            i += 1
+            continue
+        else:
+            new_string = new_string + my_string[i]
+            i += 1
+    return new_string

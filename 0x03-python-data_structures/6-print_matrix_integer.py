cb2524a5052c3872374a5c306427492a9b59a1c1
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-05 02:24:17 +0300

    removed spaces end of file
    I remove the spaces in the end of a line
    by first checking if I am at the end of
    a file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,9 @@ def print_matrix_integer(matrix=[[]]):
     while i < len(matrix):
         j = 0
         while j < len(matrix[i]):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != len(matrix[i]) - 1:
+                print(" ", end="")
             j += 1
         print()
         i += 1

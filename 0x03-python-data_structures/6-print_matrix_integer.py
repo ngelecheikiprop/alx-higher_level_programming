e09dd757bb5194ab3bab20d1c26c4d54e9730469
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-05 02:14:23 +0300

    completed task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    j = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            print("{:d}".format(matrix[i][j]), end=" ")
+            j += 1
+        print()
+        i += 1

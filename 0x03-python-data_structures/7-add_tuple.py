33ef7b7a26719ba392f7b2e391dc45ffeb322aa2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-07 06:02:32 +0300

    completed number 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = 0
-    b = 0
-    if tuple_a[0]:
-        a += tuple_a[0]
-    if tuple_b[0]:
-        a += tuple_b[0]
-    if tuple_a[1]:
-        b += tuple_a[1]
-    if tuple_b[1]:
-        a += tuple_b[1]
-    return tuple(a, b)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple

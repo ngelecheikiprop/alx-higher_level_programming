16575a1fb893842bcbd414e1aff6973bfb29b588
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-08 05:05:07 +0300

    worked on edge case of null list
    sorted out the edge case where we have null

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     i = 0
     j = -1
+    if not my_list:
+        return
     while (i < len(my_list)):
         print("{:d}".format(my_list[j]), end="\n")
         j -= 1

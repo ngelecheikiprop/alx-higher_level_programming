3e6db2f98f82cc8aa82416cb44f58957dce15d56
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-09 05:49:55 +0300

    modified task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -24,8 +24,8 @@ def an_to_int(roman_string):
                 if rom_n.get(ch) <= last_rom:
                     num += to_subtract(list_num)
                     list_num = [rom_n.get(ch)]
-                else:
-                    list_num.append(rom_n.get(ch))
-                    last_rom = rom_n.get(ch)
-                    num += to_subtract(list_num)
+            else:
+                list_num.append(rom_n.get(ch))
+                last_rom = rom_n.get(ch)
+                num += to_subtract(list_num)
     return (num)

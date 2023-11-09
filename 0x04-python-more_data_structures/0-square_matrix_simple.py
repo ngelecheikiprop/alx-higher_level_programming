886588ec6e3ae858dfd2eda0baf0470e07a14900
ngelecheikiprop <ngelecheikiprop@gmail.com>
2023-11-09 05:11:50 +0300

    removed one interpreter line repeated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """
     wordA function that computes the square
@@ -7,6 +6,6 @@ def square_matrix_simple(matrix=[]):
     """
     new_matrix = []
     for col in matrix:
-        result = list(map(lambda x: x**2, col))
+        result = list(map((lambda x: x**2), col))
         new_matrix.append(result)
     return

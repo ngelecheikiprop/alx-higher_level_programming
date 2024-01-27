fd0d2b8fa89e8791d9f8392dcabce71566b949ba
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-27 12:05:09 +0300

    changed the way next line is added

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 if (number > 0):
-    print("{:d} is positive\n".format(number))
+    print("{:d} is positive".format(number))
 elif (number == 0):
-    print("{} is zero\n".format(number))
+    print("{} is zero".format(number))
 elif (number < 0):
-    print("{} is negative\n".format(number))
+    print("{} is negative".format(number))
 

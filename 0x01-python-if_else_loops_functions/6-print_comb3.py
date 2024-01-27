285a251a3d04640205e9a32abfc620bcb6889fe1
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-27 15:56:40 +0300

    a program that prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''offset = 2;
+for num in range(1,100):
+    if (num % 10 == 0):
+        num += offset
+        offset += 1
+    else:
+        num += offset
+    if (num < 90):
+        print(num)
+    else:
+        break'''
+printed = 0
+for i in range(10):
+    for j in range(i+1, 10):
+        if (i == 8 and j == 9):
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end = "")
+

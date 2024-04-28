4b3cf7fa6f0e771de07a354326ca2df1778f2126
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 08:44:58 +0300

    removed typo and corrected pycodestyle errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None of len(list(a_dictionary)) == 0:
+    if a_dictionary is None or len(list(a_dictionary)) == 0:
         return None
     key1 = list(a_dictionary)[0]
     large = a_dictionary[key1]
     large_key = key1
     for key in list(a_dictionary):
         if a_dictionary[key] > large:
-                large = a_dictionary[key]
-                large_key = key
+            large = a_dictionary[key]
+            large_key = key
     return large_key

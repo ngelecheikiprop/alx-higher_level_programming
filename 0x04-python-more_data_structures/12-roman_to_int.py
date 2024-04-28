8899098844ee6a30828df32e78298039890e66f5
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 09:45:59 +0300

    checking the type of strng before processing"

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     number = 0
-    roman_dict =  {"I":1,"V":5, "X":10,"L":50, "C":100, "D":500, "M":1000}
-
+    roman_dict =  {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     i = 0
+
+    if type(roman_string) is not str:
+        return 0
     while i < len(roman_string):
         value = roman_dict[roman_string[i]]
         if(i + 1 < len(roman_string)):

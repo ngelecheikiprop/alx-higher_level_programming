a5a1695c35c5889a6a619f06c91e3005bd2217b2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 08:35:30 +0300

    removed bugs

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,9 @@ def best_score(a_dictionary):
         return None
     key1 = list(a_dictionary)[0]
     large = a_dictionary[key1]
+    large_key = key1
     for key in list(a_dictionary):
         if a_dictionary[key] > large:
+                large = a_dictionary[key]
                 large_key = key
     return large_key

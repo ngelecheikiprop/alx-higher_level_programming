1a1f78b323d7535f3500b83a4541e44a54ef386a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 08:43:21 +0300

    handled an empty dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None of len(list(a_dictionary)) == 0:
         return None
     key1 = list(a_dictionary)[0]
     large = a_dictionary[key1]

557c2d2f7bcaa5765d6ba9b4de415bc4b0fc77c3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-29 01:25:14 +0300

    setters and getters

--- 0x06-python-classes/examples/learn_to_program.py
+++ 0x06-python-classes/examples/learn_to_program.py
@@ -15,10 +15,48 @@ class Dog:
     def barks(self):
         print("{} the dog barks".format(self.name))
 
-def main():
-    spot = Dog("Spot", 66,26)
-    spot.barks()
+spot = Dog("Spot", 66,26)
+spot.barks()
 
-    bowser = Dog()
-    bowser.barks()
-main()
+bowser = Dog()
+bowser.barks()
+
+
+class Square:
+
+    def __init__(self, height="0", width="0"):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        print("Retrieving the heigh")
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value.isdigit():
+            self.__height = value
+        else:
+            print("enter a number for height")
+
+    def width(self):
+        print("Retrieving the heigh")
+        return self.__width
+
+    @height.setter
+    def width(self, value):
+        if value.isdigit():
+            self.__width = value
+        else:
+            print("enter a number for height")
+
+    def get_area(self):
+        return int(self.__height) * int(self.__width)
+
+asquare = Square()
+
+asquare.height = input("enter height :")
+asquare.width = input("enter width : ")
+
+print("area of {} by {} is {}".format(asquare.height, asquare.width, asquare.get_area()))

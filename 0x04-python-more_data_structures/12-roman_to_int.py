e1375c2fe385cf6a1d4b4a212357b3ecc455dd7a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 09:20:48 +0300

    handles ix and iv

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     number = 0
     roman_dict =  {"I":1,"V":5, "X":10,"L":50, "C":100, "D":500, "M":1000}
+    if "IV" in roman_string or "IX" in roman_string:
+        number = number -2
     for c in roman_string:
         value = roman_dict[c]
         number += value

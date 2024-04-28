99315b669a2a55800c2c8dc26e5f53a004684940
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-28 21:41:44 +0300

    using decorators, so amaizing

--- 0x06-python-classes/examples/python_course.py
+++ 0x06-python-classes/examples/python_course.py
@@ -12,3 +12,28 @@ print( p1.get_x())
 
 p1.set_x(47)
 print( p1.get_x())
+
+#using the propoerty decorator
+print("#using the propoerty decorator")
+print("-------------------")
+class P:
+
+    def __init__(self, x):
+        self.x = x
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self.__x = 0
+        elif x > 1000:
+            self.__x = 1000
+        else:
+            self.__x = x
+
+p1 = P(1001)
+print(p1.x)
+print("wooow")

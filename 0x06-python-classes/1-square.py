8a0413c1b0b11d9d9dde8335bcbfc57181fd1ba9
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-29 03:33:02 +0300

    added the dc of class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,12 @@
 Contains a class square
 """
 class Square:
+    """
+     class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+
+    """
     def __init__(self, size):
         self.__size = size

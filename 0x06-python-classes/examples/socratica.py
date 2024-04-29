76b0f062f554e57d569e0083082100759ff3e8f5
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-04-29 03:24:00 +0300

    examples from socratica

---
+++ 0x06-python-classes/examples/socratica.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Examples from the youtbe channe socratica
+https://www.youtube.com/watch?v=apACNr7DC_s
+"""
+class User:
+    pass
+
+user1 = User()
+user2 = User()
+
+user1.first_name = ("Dave")
+user1.last_name = ("Bowman")
+print(user1.first_name)

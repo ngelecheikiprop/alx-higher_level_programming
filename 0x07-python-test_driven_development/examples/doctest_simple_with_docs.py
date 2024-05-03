56ac61f2c294921bec7a55ed48008ad45a15567f
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-03 13:46:11 +0300

    using doctest simple with doctest

---
+++ 0x07-python-test_driven_development/examples/doctest_simple_with_docs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def my_function(a, b):
+    """ Returns a * b
+    works with numbers:
+    >>> my_function(2, 4)
+    8
+    >>> my_function(2, 7)
+    14
+
+    and strings
+    >>> my_function('a', 3)
+    'aaa'
+    """
+    return a * b
+

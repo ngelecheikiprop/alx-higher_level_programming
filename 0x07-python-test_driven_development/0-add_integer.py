f2c67b1e2a4183e494da0c5251a120c68a9af86a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-04 16:49:32 +0300

    added a module docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""This module has the function 0-add_integer add_integer
+to do addition
+"""
 def add_integer(a, b=98):
     """Adds two intergers"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
+
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+
+    if type(b) is float:
+        b = int(b)
 
     return a + b

d0af3d4fbd3dc1860ef9639becb794cdf81123d2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-04 17:09:02 +0300

    remoed extra lines in doctsrings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,22 +2,13 @@
 """This module has the function 0-add_integer add_integer
 to do addition.
 a function that adds 2 integers.
-
 Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b
-You are not allowed to import any module
 """
 
 
 def add_integer(a, b=98):
     """a function that adds 2 integers.
-
-Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
-a and b must be first casted to integers if they are float
-Returns an integer: the addition of a and b
+    Args : a, b whis is 98 by defualt
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")

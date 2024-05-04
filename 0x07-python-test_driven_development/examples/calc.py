0b4cc4a07f661327d390d457203ce5d17f0de16e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-04 12:13:16 +0300

    simple calculations functions

---
+++ 0x07-python-test_driven_development/examples/calc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add(x, y):
+    """Adds x and y"""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts y from x"""
+    return x - y
+
+def multiply(x, y):
+    """multiplies x and y"""
+    return x * y
+
+def divide(x, y):
+    """Divides x and y"""
+    return x / y
+

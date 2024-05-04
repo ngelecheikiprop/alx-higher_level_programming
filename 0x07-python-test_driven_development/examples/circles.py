f796714d6e692dd395ff0d69ec7aae1d9f5a3fd5
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-04 11:55:55 +0300

    learning from socratica in youtube

--- 0x07-python-test_driven_development/examples/circles.py
+++ 0x07-python-test_driven_development/examples/circles.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+from math import pi
 def circle_area(r):
+    if r < 0:
+        raise ValueError("the radius cannot be negative.")
+    if type(r) not in [int, float]:
+        raise TypeError("The radius must be a non negative real number")
     return pi*(r**2)
 

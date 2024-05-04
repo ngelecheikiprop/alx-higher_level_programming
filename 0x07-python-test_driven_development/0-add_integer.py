9e70300bc1f89f18ecdf25dd77ff2e9e466f4ee0
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-04 17:00:31 +0300

    increased number of lines of doc strings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module has the function 0-add_integer add_integer
-to do addition
+to do addition.
+a function that adds 2 integers.
+
+Prototype: def add_integer(a, b=98):
+a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b
+You are not allowed to import any module
 """
 
 
 def add_integer(a, b=98):
-    """Adds two intergers"""
+    """a function that adds 2 integers.
+
+Prototype: def add_integer(a, b=98):
+a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
 

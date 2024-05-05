799a633186821c4294e65199971f44934bb439ec
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-05 11:40:01 +0300

    upgraded checks for max_interger function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,3 +13,9 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-7, 1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([]), None)

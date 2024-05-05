7a2fb575ac130c262261ac79ca0fca3ee9fd701c
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-05 16:26:02 +0300

    tried the third example on the tutorial

--- 0x08-python-more_classes/examples/python-course.py
+++ 0x08-python-more_classes/examples/python-course.py
@@ -30,7 +30,46 @@ class Robot:
         self.build_year = build_year
 
 for number, text in enumerate(Robot.Three_Laws):
-    print(f"{number}:{text}")
+    #print(f"{number}:{text}")
+    pass
 
-#third example
+#third example tried myslef
+class Car:
+    counter = 0
+    def __init__(self, name):
+        self.name = name
+        Car.counter += 1
+        #print("just made {}".format(self.name))
 
+    def sell_car(self):
+        Car.counter -= 1
+        #print("sold {}".format(self.name))
+
+lambo = Car("lambo")
+audi = Car("audi")
+#print("expecting two cars")
+#print(Car.counter)
+lambo.sell_car()
+#print(Car.counter)
+audi.sell_car()
+#print(Car.counter)
+
+#third example in tutorial
+class C:
+
+    counter = 0
+
+    def __init__(self):
+        type(self).counter += 1
+
+    def __del__(self):
+        type(self).counter -= 1
+
+x = C()
+print(C.counter)
+y = C()
+print(C.counter)
+del x
+print(C.counter)
+del y
+print(C.counter)

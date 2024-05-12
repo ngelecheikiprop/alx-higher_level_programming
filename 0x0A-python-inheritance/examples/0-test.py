5947261dfd11f4989170b156f87f47dbbd633ad6
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-12 06:38:37 +0300

    I just learned how to use inheritance

--- 0x0A-python-inheritance/examples/0-test.py
+++ 0x0A-python-inheritance/examples/0-test.py
@@ -9,5 +9,16 @@ class Person(object):
         print(self.name, self.id)
 
 
-emp = Person("Satyam", 102)
-emp.display()
+# emp = Person("Satyam", 102)
+# emp.display()
+
+class Emp(Person):
+
+    def print(self):
+        print("Emp class called")
+
+
+emp_details = Emp("Maynak", 103)
+
+emp_details.display()
+emp_details.print()

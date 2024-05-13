5efc6504fdad382cfc186507591d16831a1b760a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-13 14:19:42 +0300

    answered number 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class host module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")

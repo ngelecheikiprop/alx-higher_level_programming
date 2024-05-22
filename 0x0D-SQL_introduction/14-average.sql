b068a40f542ef0a0390256a092b69690e555c997
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-22 13:36:44 +0300

    number 14 trial

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- gets the score average and puts it in a new column called average
+ALTER TABLE second_table ADD average FLOAT;
+INSERT INTO second_table (average)
+SELECT AVG(score) FROM second_table;

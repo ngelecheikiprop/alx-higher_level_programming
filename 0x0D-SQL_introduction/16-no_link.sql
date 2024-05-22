ef9d20018ceb37fd8310deedb177be32098b5043
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-22 15:49:15 +0300

    choosed the columns I want well

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists where name is not null and arranges in descenind score
-SELECT * FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

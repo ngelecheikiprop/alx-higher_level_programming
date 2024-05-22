3b6a82f45dcea78a401f0163a5b0347a736fdae0
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-22 15:54:57 +0300

    namesd the column average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- gets the score average and puts it in a new column called average
-ALTER TABLE second_table ADD average FLOAT;
-INSERT INTO second_table (average)
-SELECT AVG(score) FROM second_table;
+-- ALTER TABLE second_table ADD average FLOAT;
+-- INSERT INTO second_table (average)
+SELECT AVG(score) AS average FROM second_table;

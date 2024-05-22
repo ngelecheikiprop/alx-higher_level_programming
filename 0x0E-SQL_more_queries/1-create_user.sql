73b899c2e879c6c1d8005f0bb7305b30fc858c46
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-22 20:08:42 +0300

    corrected number 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates a new user user_0d-1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost;

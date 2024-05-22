b73cf4ea41b3202b5829a51afc3b906db79f31c7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-22 12:13:29 +0300

    corrected a minor error of not ending line with ;

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- script that inserts new row
 -- in the table firsttable
-INSERT INTO first_table (id, name) VALUES (80, 'Best School')
+INSERT INTO first_table (id, name) VALUES (80, 'Best School');

8a9cc5bcc836c993c18449950a2a8996bb68d2f2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-23 08:45:23 +0300

    corrected number 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
--- ists all cities contained in the database hbtn_0d_usa
-SELECT id, name
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id AS id, states.name AS name, states.name AS name
 FROM cities
-NATURAL JOIN states
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY id ASC;

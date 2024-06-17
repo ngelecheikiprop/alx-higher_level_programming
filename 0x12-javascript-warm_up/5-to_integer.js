d2fa8ae72741a594a718711ba8c94bd851e456f0
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-17 20:29:54 +0000

    trial 2: number 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const input = process.argv[2]
 
 if (Number(input)) {
-  console.log('My number: ',Number(input));
+  console.log('My number: ',parseInt(input));
 }
 else {
   console.log('Not a number');

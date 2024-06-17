4c46fb3217d9f4c2643f3e720606db302eccc037
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-17 19:44:53 +0000

    handled many arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const argvLen = process.argv.length;
 if (argvLen <= 2) {
   console.log('No argument');
-} else {
+} else if (argvLen === 3) {
   console.log('Argument found');
+} else {
+    console.log('Arguments found');
 }

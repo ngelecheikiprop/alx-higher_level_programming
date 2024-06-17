73a3302c828b4d66680453eae5803d25f564bb18
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-17 19:41:41 +0000

    removed erros of semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const argv_len = process.argv.length
-if (argv_len <= 2){
-    console.log('No argument')
-}
-else {
-    console.log('Argument found')
+const argvLen = process.argv.length;
+if (argvLen <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
 }

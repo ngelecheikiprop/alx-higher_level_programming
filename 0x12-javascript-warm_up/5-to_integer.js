10dcd0b2379944c446c55c7e5df6b42cc72a8b4f
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 02:51:16 +0000

    fast fprward

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const input = process.argv[2]
-
-if (Number(input)) {
-  console.log('My number: ',parseInt(input));
-}
-else {
-  console.log('Not a number');
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);

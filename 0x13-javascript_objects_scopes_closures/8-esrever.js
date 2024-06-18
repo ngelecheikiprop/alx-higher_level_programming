bce259b9be249ee5224edd52a5e7115265765b84
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 18:05:20 +0000

    semistandard error correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const len = list.length;
-  let reversedList = [];
+  const reversedList = [];
   for (let i = len - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
   return reversedList;
-}
+};

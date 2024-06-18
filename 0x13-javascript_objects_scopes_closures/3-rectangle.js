26327b6b6ef55f489cc85f642ce124f499912da9
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 14:22:46 +0000

    printing out a rectangle function

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let text = '';
+    for (let i=0; i <this.height; i++) {
+      for (let j=0; j < this.width; j++) {
+        text += 'X';
+      }
+      if (i !== this.height - 1) {
+        text += '\n';
+      }
+    }
+    console.log(text);
+  }
+}
+module.exports = Rectangle;

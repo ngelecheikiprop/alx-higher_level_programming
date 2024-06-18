9b2531637d798ee327220c45aa6c73b101f98a70
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 14:49:41 +0000

    number 6: square inheriting from square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const Square4 = require('./5-square');
+class Square extends Square4 {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
+    if (c) {
+      let text = '';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          text += c;
+        }
+        if (i !== this.height - 1) {
+          text += '\n';
+        }
+      }
+      console.log(text);
+    }
+    else {
+      super.print();
+    }
+  }
+}
+
+module.exports = Square;

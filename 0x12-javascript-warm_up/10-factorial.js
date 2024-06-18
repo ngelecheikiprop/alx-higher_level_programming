a3c7b6b4cf3b52ee5b10627d5964a42e2060a157
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 02:46:10 +0000

    fast fprward

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const input = parseInt(process.argv[2]);
-function factorial (number) {
-  if (number === 1) {
-    return 1;
-  }
-  return number * factorial(number - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(input));
+console.log(factorial(Number(process.argv[2])));

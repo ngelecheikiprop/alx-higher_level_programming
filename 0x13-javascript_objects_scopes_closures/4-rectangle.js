120bb5a767aeafe279ef91d1e64b81ac2d75b4a5
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-18 14:31:50 +0000

    added double and rotate features

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let text = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        text += 'X';
+      }
+      if (i !== this.height - 1) {
+        text += '\n';
+      }
+    }
+    console.log(text);
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;

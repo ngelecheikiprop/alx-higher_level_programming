4131853be45839f15881aa1da9a180fa697407da
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-20 10:12:52 +0000

    number 0 take user from command line

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import MySQLdb
-db = MySQLdb.connect("localhost", "root", "", "hbtn_0e_0_usa")
+import sys
+db = MySQLdb.connect(f"localhost", "${sys.argv[1]}", "${sys.argv[2]}", "${sys.argv[3]}")
 cur = db.cursor()
-cur.execute("""USE hbtn_0e_0_usa""")
+cur.execute(f"""USE ${sys.argv[3]}""")
 cur.execute("""SELECT * FROM states""")
 query_rows = cur.fetchall()
 for row in query_rows:

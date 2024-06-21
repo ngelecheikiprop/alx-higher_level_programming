dacf49630d7f53018296d1fa284652a274ff9f98
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 10:30:52 +0000

    number 9, finding states that have number a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""print the states table"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
+    session.close()

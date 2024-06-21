d0dc19fd18241e6ae9f91cf66f89e68cf596e24e
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 04:16:17 +0000

    added tow line documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""script that lists all states
+from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 db = MySQLdb.connect(f"localhost", "${sys.argv[1]}", "${sys.argv[2]}", "${sys.argv[3]}")

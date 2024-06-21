993e305f52ac416ae2ff7a6a862b263b433a2026
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 09:28:39 +0000

    number 7, getting states using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+  from sqlalchemy import create_engine, Column, Integer, String
+  from sqlalchemy.ext.declarative import declarative_base
+  from sqlalchemy.orm import sessionmaker
+  from model_state import Base, State
+  engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format("root", "root", "hbtn_0e_0_usa"), pool_pre_ping=True)
+  Session = sessionmaker(bind=engine)
+  Base.metadata.create_all(engine)
+  session = Session()
+  for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+      print("{}: {}".format(state.id, state.name))
+  session.close()

1caca886319eabaadd2ab42ea72e9e139d3e7334
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-21 08:39:09 +0300

    removed styling errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,11 +4,16 @@ from the database hbtn_0e_0_usa"""
 if __name__ == "__main__":
     """run when function is called"""
     import MySQLdb
-    import sys 
+    import sys
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host='localhost',port=3306, user=username, passwd=password, db=database) 
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states""")
     states = cur.fetchall()

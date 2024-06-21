e9402269de23ed282487d498ffb9b466452ce4d7
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 10:52:57 +0000

    number 10, removed index erros

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Print the states table"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+
+    # Query for the state with the given name
+    state = session.query(State).filter_by(name=state_name).first()
+
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()

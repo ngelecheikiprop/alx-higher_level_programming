86bc3b21f02ba42cfc775f684ebff0f4160f3b3b
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 11:33:29 +0000

    number 11, add a new user

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Print the states table"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    new_State = State()
+    new_State.name = "Louisiana"
+    session.add(new_State)
+    session.commit()
+    our_user = session.query(State).filter_by(name='Louisiana').first()
+    print(our_user.id)
+    session.close()

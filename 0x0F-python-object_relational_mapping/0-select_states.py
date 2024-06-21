50a082b3ea08a70314ef687cf028ff6c4f47f8d4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-21 08:11:12 +0300

    added the port details

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 username = sys.argv[1]
 password = sys.argv[2]
 database = sys.argv[3]
-db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database) 
+db = MySQLdb.connect(host='localhost',port=3306, user=username, passwd=password, db=database) 
 cur = db.cursor()
 cur.execute("""SELECT * FROM states""")
 states = cur.fetchall()

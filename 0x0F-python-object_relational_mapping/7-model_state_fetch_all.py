365c3416dac6562bc38a55a45c4b935b07f1d45c
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 09:43:21 +0000

    no  more pep styling issues

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """print the states table"""
-if __name__=="__main__":
-  from sqlalchemy import create_engine
-  from sqlalchemy.ext.declarative import declarative_base
-  from sqlalchemy.orm import sessionmaker
-  from model_state import Base, State
-  engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format("root", "root", "hbtn_0e_0_usa"), pool_pre_ping=True)
-  Session = sessionmaker(bind=engine)
-  Base.metadata.create_all(engine)
-  session = Session()
-  for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
-      print("{}: {}".format(state.id, state.name))
-  session.close()
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        "root", "root", "hbtn_0e_0_usa"), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    # HERE: no SQL query, only objects!
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()

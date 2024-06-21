1a496a5340d4238cb0442f674a30e7de2504a63e
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 09:41:20 +0000

    removed erros in pep styling

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """has the states table model and the instance Base"""
-from sqlalchemy import  Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
+
+
 class State(Base):
     """This is class to model the states table"""
     __tablename__ = 'states'

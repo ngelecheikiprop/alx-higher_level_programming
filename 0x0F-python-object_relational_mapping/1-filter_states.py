40dd7f655f18efcaeba67b136fe4666e8a125322
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 06:22:28 +0000

    order by ascending order of id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         passwd=password,
         db=database)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%';""")
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;""")
     states = cur.fetchall()
     for state in states:
         print(state)

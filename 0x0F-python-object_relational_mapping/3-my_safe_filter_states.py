c659aef8fb4b05954c1e4ebeaa4b269961559a07
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-21 07:36:52 +0000

    made safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         db=database)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states
-                WHERE name='{}'
+                WHERE name=\'{}\'
                 ORDER BY id ASC;""".format(name))
     states = cur.fetchall()
     for state in states:

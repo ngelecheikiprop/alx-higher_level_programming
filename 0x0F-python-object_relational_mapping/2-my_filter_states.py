f42634d6f5507bb75b298c6fe1d9cf81196fb8c9
ngelecheikiprop <136060866+ngelecheikiprop@users.noreply.github.com>
2024-06-23 14:13:18 +0000

    number 2, changed the sql command

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         passwd=password,
         db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
                 .format(name))
     states = cur.fetchall()
     for state in states:

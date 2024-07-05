bd69e0c86839c4763cbf9f94f38e1e5d8e62be14
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-05 21:40:31 +0300

    number 7, removed bugs

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import requests
     import sys
-    url = sys[1]
+    url = sys.argv[1]
     response = requests.get(url)
     if response.status_code == 200:
         print(response.text)

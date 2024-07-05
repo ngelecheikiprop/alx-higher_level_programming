29ac83ab492db87a8b98106c178b71111876740a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-05 14:53:19 +0300

    number 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+    import urllib.request
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        data = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))

317bc42dc2129361f83d4af7b8dccc6fbe48a98e
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-05 21:39:14 +0300

    number 7, printing error codes more than 400

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
-"""
-import sys
-import requests
-
-
+'if errors print the ones over 400'
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    import requests
+    import sys
+    url = sys[1]
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
+    elif response.status_code > 400:
+        print("Error code:", response.status_code)

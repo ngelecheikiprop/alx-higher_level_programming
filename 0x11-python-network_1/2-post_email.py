c9e2e36af97d24938b2f0e9956efbc3539ba09d8
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-05 18:19:19 +0300

    number 2. removed styling errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
     url = sys.argv[1]
     email = sys.argv[2]
-    value = {"email":email}
+    value = {"email": email}
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)

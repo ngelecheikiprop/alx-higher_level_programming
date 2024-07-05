1736584942be370afdb96c23d5be29fc2bea5cdb
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-05 20:45:17 +0300

    number 3: handling erros and accepting inputs

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""
-A script that:
-- takes in a URL,
-- sends a request to the URL and displays the body of the response
-- (decoded in utf-8).
-- manages urllib.error.HTTPError exceptions and prints:
-  "Error code: " followed by the HTTP status code
-"""
-import sys
-import urllib.request
-import urllib.error
-
 if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
     url = sys.argv[1]
-
+    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print("Error code: ", e.status)

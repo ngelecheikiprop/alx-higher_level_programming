b4f991fe951295fbc74c75845f72b5f6923a4ed7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-06 01:48:52 -0700

    removed styling errors and added doc sting

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+'getting data then checking it'
 if __name__ == "__main__":
     import requests
     import sys
-    if  len(sys.argv) == 2:
+    if len(sys.argv) == 2:
         q = sys.argv[1]
     else:
         q = ""
     url = 'http://0.0.0.0:5000/search_user'
-    data = {"q":q}
+    data = {"q": q}
     response = requests.post(url, data)
     data = response.json()
-    if not  isinstance(data, dict):
+    if not isinstance(data, dict):
         print("Not a valid JSON")
     if not data:
         print("No result")

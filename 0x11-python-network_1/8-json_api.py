f5e8dcadc94a8d419be4403c6127ab4090cbd732
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-09 22:32:18 +0300

    number 8, detecting value error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-'getting data then checking it'
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+
 if __name__ == "__main__":
-    import requests
-    import sys
-    if len(sys.argv) == 2:
-        q = sys.argv[1]
-    else:
-        q = ""
-    url = 'http://0.0.0.0:5000/search_user'
-    data = {"q": q}
-    response = requests.post(url, data)
-    data = response.json()
-    if response.headers.get('Content-Type') !=  "application/json":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
-    elif not data:
-        print("No result")
-    else:
-        print("[{}] {}".format(data.get('id'), data.get('name')))

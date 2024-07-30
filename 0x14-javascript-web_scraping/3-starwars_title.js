c667b39c10f00d3e8cbe376e5544978d253c81cb
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-30 18:51:48 +0300

    completed number 3
    gets the title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request')
+const id = process.argv[2]
+request.get(`https://swapi-api.alx-tools.com/api/films/${id}`,(err, resp, body) => {
+    console.log(JSON.parse(body).title)
+});

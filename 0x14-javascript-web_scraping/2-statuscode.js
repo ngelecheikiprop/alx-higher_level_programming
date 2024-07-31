f68a5f45f4605b39a7dc218e2bec5f267c877c6b
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-31 05:01:40 +0300

    removed the use of var

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-var request = require('request')
+const request = require('request')
 request.get(process.argv[2], (err, response, body)=>{
     if (err) console.log(err)
     console.log('code: ',response.statusCode)
